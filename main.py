39bf17772a1f5130aa7c0fd3114c26752ac385d9
kapoel <fokoukapoel@gmail.com>
2025-12-15 15:47:25 +0100

    feature: step2, cost_total of rebuilding

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from config import *
 
 #Import data
 network_df = pd.read_excel("./data/reseau_en_arbre.xlsx")
@@ -11,7 +12,27 @@ def clean_data(network_df,infra_df,building_df):
     infra_df = pd.merge(network_df,infra_df, how='left', left_on='infra_id', right_on='id_infra')
     infra_df.drop(columns=['infra_id','nb_maisons','infra_type'],inplace=True)
     data_cleaned = pd.merge(infra_df,building_df,how='left',on='id_batiment')
+    data_cleaned.drop_duplicates(inplace=True)
     return data_cleaned
 
+def rebuilding_cost(dataframe):
+    total_amount=0    
+    for _,line in dataframe.iterrows():
+        longueur = line['longueur']
+        if line['type_infra'] == 'aerien':
+            total_amount += longueur*aerian_cost_per_meter
+        elif line['type_infra'] == 'semi-aerien':
+            total_amount += longueur*semi_aerian_cost_per_meter
+        elif line['type_infra'] == 'fourreau':
+            total_amount += longueur*duct_cost_per_meter
+        else:
+            print(f"valeur non prise en charge{_,line['type_infra']}")
+    return total_amount
 
-print(clean_data(network_df,infra_df,building_df))
+
+
+
+
+
+dataset_cleaned = clean_data(network_df,infra_df,building_df)
+print(f"le montant total est de {rebuilding_cost(dataset_cleaned):.2f} euros")
