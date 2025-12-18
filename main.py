cacabbacd84381d4f6248c849f11189dc5e98e31
kapoel <fokoukapoel@gmail.com>
2025-12-18 17:48:39 +0100

    bug_fix: correction of function rebuilding_cost

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ def clean_data(network_df,infra_df,building_df):
 
 def rebuilding_cost(dataframe):
     total_amount=0    
-    for _,line in dataframe.iterrows():
+    dataframe_selected = dataframe[['id_infra','longueur','type_infra']].drop_duplicates('id_infra')
+    for _,line in dataframe_selected.iterrows():
         longueur = line['longueur']
         if line['type_infra'] == 'aerien':
             total_amount += longueur*aerian_cost_per_meter
@@ -48,7 +49,7 @@ def time_to_rebuild_hospital(dataframe):
 
 def create_objects(dataframe):   
     for infra in set(dataframe.id_infra):
-        longueur = dataframe[dataframe['id_infra']==infra].longueur
+        longueur = dataframe[dataframe['id_infra']==infra].longueur.values[0]
         nb_maisons = dataframe[dataframe['id_infra']==infra].nb_maisons.sum()
   
         infras[infra] = Infra(infra_id=infra,
@@ -57,19 +58,18 @@ def create_objects(dataframe):
                               infra_state='a_remplacer')
     
     for building in set(dataframe.id_batiment):
-        id_building = building
         infras_buildings = list(dataframe[dataframe['id_batiment']==building].id_infra.values)
         liste_infras = [infras[infra] for infra in infras_buildings]
-        buildings[id_building] = Buildings(id_building = id_building, liste_infras = liste_infras )
+        buildings[building] = Buildings(id_building = building, liste_infras = liste_infras )
 
     
 
 dataset_cleaned = clean_data(network_df,infra_df,building_df)
-# print(dataset_cleaned)
-infras={}
-buildings = {}
-create_objects(dataset_cleaned)
-print(buildings['E000003'])
-
-#print(time_to_rebuild_hospital(dataset_cleaned))
-# print(f"le montant total est de {rebuilding_cost(dataset_cleaned):.2f} euros")
+# print(dataset_cleaned.head(20))
+# dataset_cleaned.to_csv('dataset.csv')
+# infras={}
+# buildings = {}
+# create_objects(dataset_cleaned)
+# print(infras['P000462'].get_infra_difficulty())
+print(f"le montant total est de {rebuilding_cost(dataset_cleaned):.2f} euros")
+print(f"le temps de reparation des infrastructures pour atteindre l'hopital est de: {time_to_rebuild_hospital(dataset_cleaned)}")
